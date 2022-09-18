056678eaceb5d579de4e16bcc7358b86193698fb
Andrus Asumets <andrus.asumets@gmail.com>
2022-09-18 09:07:30 +0300

    refactor freesound crawler

--- freesound-crawler/app.js
+++ freesound-crawler/app.js
@@ -1,8 +1,9 @@
 const puppeteer = require("puppeteer");
 
-(async () => {
-  const MAX_PAGES = 10000
+const MAX_PAGES = 10000
+const downloadPath = '/Volumes/LaCie/freesound-crawler/downloads';
 
+const logIn = async () => {
   const browser = await puppeteer.launch({ devtools: false });
   const page = await browser.newPage();
   await page.goto('https://freesound.org/home/login/?next=/');
@@ -13,14 +14,22 @@ const puppeteer = require("puppeteer");
   });
 
   await page.waitForTimeout(3000);
+  return page;
+};
 
+const getPaginationCount = async (page) => {
   await page.goto('https://freesound.org/browse/packs/?order=-num_downloads');
   const pagePaginationCount = await page.evaluate(async () => {
     const pagePaginationCount = parseInt($('.last-page a')[0].text);
     return pagePaginationCount;
   });
 
-  let pageIndex = 1144;
+  return pagePaginationCount;
+};
+
+const loadPages = async (page, pagePaginationCount) => {
+  let pageIndex = 0;
+
   while (pageIndex != pagePaginationCount && pageIndex < MAX_PAGES) {
     await page.goto(`https://freesound.org/browse/packs/?order=-num_downloads&page=${pageIndex + 1}#pack`);
     const { packUrlsPerPage } = await page.evaluate(async () => {
@@ -29,63 +38,81 @@ const puppeteer = require("puppeteer");
       return { packUrlsPerPage }
     });
 
-    let packIndex = 0
-    while (packIndex != packUrlsPerPage.length) {
-      await page.goto(packUrlsPerPage[packIndex]);
-      const { packPaginationCount } = await page.evaluate(async () => {
-      const packPaginationCount = $('.other-page').length / 2 + 1;
-        const hasSamples = $("img[src$='/media/images/licenses/nolaw.png']").length > 0;
-        if (!hasSamples) {
-          return { packPaginationCount: 0 }
-        }
-
-        return { packPaginationCount } ;
-      });
-
-      if (packPaginationCount == 0) {
-        packIndex = packIndex + 1;
-        continue;
-      }
-      
-      let packPaginationIndex = 1;
-      while (packPaginationIndex != packPaginationCount) {
-        const packPaginationUrl = `${packUrlsPerPage[packIndex]}?page=${packPaginationIndex + 1}#sound`;
-        await page.goto(packPaginationUrl);
-        const { soundUrls } = await page.evaluate(async () => {
-          const nolawIds = $('.cc_license').toArray().map(img => {
-            if (img.src.includes("nolaw")) {
-              const parent = $(img).parent("div").parent("div").parent("div");
-              const nolawId  = $(parent)['0'].id;
-              return nolawId;
-            }
-          }).filter(nolawId => nolawId);
-          
-          const soundUrls = $('.sample_player_small').toArray().map(player => {
-            const playerId = $(player).context.id;
-            const url = $("#" + playerId).find(".title")[0].href;
-            return nolawIds.includes(playerId) ? url : null;
-          }).filter(nolawId => nolawId);
-
-          return { soundUrls };
-        });
-
-        let soundIndex = 0;
-        while (soundIndex != soundUrls.length) {
-          const soundUrl = soundUrls[soundIndex];
-          await page.goto(soundUrl);
-          await page._client.send('Page.setDownloadBehavior', { behavior: 'allow', downloadPath: '/Volumes/LaCie/freesound-crawler/downloads' });
-          await page.click('#download_button');
-          soundIndex = soundIndex + 1;
-        }
-        
-        packPaginationIndex = packPaginationIndex + 1;
-      }
-
-      packIndex = packIndex + 1
-    }
-
+    await loadPacks(page, packUrlsPerPage);
     pageIndex = pageIndex + 1
-
     console.log({ pagePaginationCount, pageIndex });
   }
+};
+
+const loadPacks = async (page, packUrlsPerPage) => {
+  let packIndex = 0
+
+  while (packIndex != packUrlsPerPage.length) {
+    await page.goto(packUrlsPerPage[packIndex]);
+    const { packPaginationCount } = await page.evaluate(async () => {
+    const packPaginationCount = $('.other-page').length / 2 + 1;
+      const hasSamples = $("img[src$='/media/images/licenses/nolaw.png']").length > 0;
+      if (!hasSamples) {
+        return { packPaginationCount: 0 }
+      }
+
+      return { packPaginationCount } ;
+    });
+
+    if (packPaginationCount == 0) {
+      packIndex = packIndex + 1;
+      continue;
+    }
+
+    await loadPack(page, packPaginationCount, packUrlsPerPage[packIndex]);
+    packIndex = packIndex + 1
+  }
+};
+
+const loadPack = async (page, packPaginationCount, packUrl) => {
+  let packPaginationIndex = 1;
+
+  while (packPaginationIndex != packPaginationCount) {
+    const packPaginationUrl = `${packUrl}?page=${packPaginationIndex + 1}#sound`;
+    await page.goto(packPaginationUrl);
+    const { soundUrls } = await page.evaluate(async () => {
+      const nolawIds = $('.cc_license').toArray().map(img => {
+        if (img.src.includes("nolaw")) {
+          const parent = $(img).parent("div").parent("div").parent("div");
+          const nolawId  = $(parent)['0'].id;
+          return nolawId;
+        }
+      }).filter(nolawId => nolawId);
+
+      const soundUrls = $('.sample_player_small').toArray().map(player => {
+        const playerId = $(player).context.id;
+        const url = $("#" + playerId).find(".title")[0].href;
+        return nolawIds.includes(playerId) ? url : null;
+      }).filter(nolawId => nolawId);
+
+      return { soundUrls };
+    });
+
+    await download(page, soundUrls);
+
+    packPaginationIndex = packPaginationIndex + 1;
+  }
+};
+
+const download = async (page, soundUrls) => {
+  let soundIndex = 0;
+
+  while (soundIndex != soundUrls.length) {
+    const soundUrl = soundUrls[soundIndex];
+    await page.goto(soundUrl);
+    await page._client.send('Page.setDownloadBehavior', { behavior: 'allow', downloadPath: downloadPath });
+    await page.click('#download_button');
+    soundIndex = soundIndex + 1;
+  }
+};
+
+(async () => {
+  const page = await logIn();
+  const pagePaginationCount = await getPaginationCount(page);
+  await loadPages(page, pagePaginationCount);
 })();
