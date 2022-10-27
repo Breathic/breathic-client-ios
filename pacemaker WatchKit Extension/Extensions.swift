cd45e65f78bfa21c81669f1e370c1ee96ee6f820
Andrus Asumets <andrus.asumets@gmail.com>
2022-10-27 20:16:08 +0300

    cleanup v2

--- pacemaker WatchKit Extension/Extensions.swift
+++ pacemaker WatchKit Extension/Extensions.swift
@@ -23,41 +23,6 @@ extension DispatchTimeInterval {
     }
 }
 
-extension Array where Element: Sequence {
-    func joined() -> Array<Element.Element> {
-        return self.reduce([], +)
-    }
-}
-
-extension Array {
-    func repeated(count: Int) -> Array<Element> {
-        assert(count > 0, "count must be greater than 0")
-
-        var result = self
-        for _ in 0 ..< count - 1 {
-            result += self
-        }
-
-        return result
-    }
-
-    static func **(lhs: Array<Element>, rhs: Int) -> Array<Element> {
-        return lhs.repeated(count: rhs)
-    }
-    
-    func chunked(into size: Int) -> [[Element]] {
-        return stride(from: 0, to: count, by: size).map {
-            Array(self[$0 ..< Swift.min($0 + size, count)])
-        }
-    }
-    
-    func indexExists(_ index: Int) -> Bool {
-      return self.indices.contains(index)
-    }
-}
-
-infix operator **
-
 struct Platform {
     static var isSimulator: Bool {
         return TARGET_OS_SIMULATOR != 0
