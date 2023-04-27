2159c83fd2ee0906538cfe29a48ec5891a36f993
Andrus Asumets <andrus.asumets@gmail.com>
2023-04-27 11:28:35 +0300

    ui fix

--- Extension/ui/views/Status.swift
+++ Extension/ui/views/Status.swift
@@ -4,39 +4,6 @@ func statusView(geometry: GeometryProxy, store: Store) -> some View {
     let columns = Array(METRIC_ORDER[0...3]).chunks(2)
 
     return VStack {
-        HStack {
-            primaryButton(
-                geometry: geometry,
-                label: "Duration",
-                value: getElapsedTime(store.state.activeSession.elapsedSeconds),
-                unit: " ",
-                valueColor: colorize("white"),
-                valueTextSize: 32,
-                borderWidth: 0,
-                isTall: false,
-                isEnabled: false,
-                minimumScaleFactor: 0.45
-            )
-
-            Spacer(minLength: 8)
-
-            primaryButton(
-                geometry: geometry,
-                label: "Distance (k" + getMetric("distance").unit + ")",
-                value: String(format: getMetric("distance").format, store.state.getMetricValue("distance") / 1000),
-                valueColor: colorize("blue"),
-                valueTextSize: 32,
-                borderWidth: 0,
-                isTall: false,
-                isEnabled: false,
-                minimumScaleFactor: 0.7
-            )
-
-            Spacer(minLength: 8)
-        }
-        
-        Spacer(minLength: 8)
-
         ForEach(columns, id: \.self) { column in
             HStack {
                 ForEach(column, id: \.self) { metric in
@@ -55,6 +22,39 @@ func statusView(geometry: GeometryProxy, store: Store) -> some View {
             }
 
             Spacer(minLength: 12)
+        }
+        
+        Spacer(minLength: 8)
+        
+        HStack {
+            primaryButton(
+                geometry: geometry,
+                label: "Distance (k" + getMetric("distance").unit + ")",
+                value: String(format: getMetric("distance").format, store.state.getMetricValue("distance") / 1000),
+                valueColor: colorize("blue"),
+                valueTextSize: 32,
+                borderWidth: 0,
+                isTall: false,
+                isEnabled: false,
+                minimumScaleFactor: 0.7
+            )
+
+            Spacer(minLength: 8)
+    
+            primaryButton(
+                geometry: geometry,
+                label: "Duration",
+                value: getElapsedTime(store.state.activeSession.elapsedSeconds),
+                unit: " ",
+                valueColor: colorize("white"),
+                valueTextSize: 32,
+                borderWidth: 0,
+                isTall: false,
+                isEnabled: false,
+                minimumScaleFactor: 0.45
+            )
+
+            Spacer(minLength: 8)
         }
     }
 }
