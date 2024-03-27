b09338bac177038235f83b625f7cef6a6886fa73
Andrus Asumets <andrusasumets@Andruss-Mini.lan>
2024-03-27 17:16:10 +0200

    add black tint to menu toolbar

--- Extension/ui/views/Toolbar.swift
+++ Extension/ui/views/Toolbar.swift
@@ -3,24 +3,25 @@ import SwiftUI
 func toolbarView(
     store: Store
 ) -> some ToolbarContent {
-        ToolbarItem(placement: .cancellationAction) {
-            Button(
-                action: {
-                    store.state.activeSubView = store.state.activeSubView != SubView.Menu.rawValue
-                        ? SubView.Menu.rawValue
-                        : MENU_VIEWS[0]
-                    store.state.activeSubView = isOverviewSelected(store: store)
-                        ? SubView.Log.rawValue
-                        : store.state.activeSubView
-                },
-                label: {
-                    Text(
-                        "☰ " + store.state.activeSubView
-                            .components(separatedBy: " (")[0] // Remove duration as well as count when overview.
-                    )
-                    .font(.system(size: 12))
-                }
-            )
-        }
+    ToolbarItem(placement: .cancellationAction) {
+        Button(
+            action: {
+                store.state.activeSubView = store.state.activeSubView != SubView.Menu.rawValue
+                    ? SubView.Menu.rawValue
+                    : MENU_VIEWS[0]
+                store.state.activeSubView = isOverviewSelected(store: store)
+                    ? SubView.Log.rawValue
+                    : store.state.activeSubView
+            },
+            label: {
+                Text(
+                    "☰ " + store.state.activeSubView
+                        .components(separatedBy: " (")[0] // Remove duration as well as count when overview.
+                )
+                .font(.system(size: 12))
+            }
+        )
+        .tint(.black)
+    }
 }
 
